3848dea67523201cf271bcc7431b3b9af2b3f4b2
marco-schneider <marco.schneider452@gmail.com>
2023-02-16 16:37:10 -0300

    Added the speed_controller

---
+++ scripts/speed_controller.js
@@ -0,0 +1,8 @@
+/*
+Esse arquivo dará conta de enviar dados para o tópico /speed_controller
+
+O tópico /speed_controller é o responsável por sinalizar ao script que forma
+a nuvem de pontos, a velocidade de aquisição dos dados. Transferindo isso
+para o nosso problema real, o tópico /speed_controller ficará responsável por
+fazer a leitura da velocidade do caminhão e comunicá-la aos demais tópicos pertinentes
+*/

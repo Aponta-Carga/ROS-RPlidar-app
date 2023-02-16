273e1e8322d3aa5c6156d91cd8be9f45127e6e3c
marco-schneider <marco.schneider452@gmail.com>
2023-02-16 01:44:36 -0300

    Initial implementation

---
+++ index.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+  console.log("Document ready!!!");
+
+
+});
+
+function sendMessage() {
+  var ros = new ROSLIB.Ros({
+    url: 'ws://localhost:9090'
+  });
+  
+  ros.on('connection', function() {
+    console.log('Connected to ROS Bridge server!');
+  });
+  
+  ros.on('error', function(error) {
+    console.log('Error connecting to ROS Bridge server:', error);
+  });
+  
+  ros.on('close', function() {
+    console.log('Disconnected from ROS Bridge server.');
+  });
+  
+  console.log("Connection established!!");
+
+  var message = new ROSLIB.Message({
+    data: 1.0
+  });
+  
+  var topic = new ROSLIB.Topic({
+    ros: ros,
+    name: '/point_cloud_controller',
+    messageType: 'std_msgs/Float32'
+  });
+  
+  topic.publish(message);
+}
+
+  var listener = new ROSLIB.Topic({
+    ros : ros,
+    name : '/listener',
+    messageType : 'std_msgs/String'
+  });
+
+  listener.subscribe(function(message) {
+    console.log('Received message on ' + listener.name + ': ' + message.data);
+    listener.unsubscribe();
+  });
